222053748163d434583466741555f32311b1a8c0
gabor_hanacsek <gabor_hanacsek@bud1eunb664420.eu.sanm.corp>
2018-07-23 14:54:06 +0200

    Beleépítve a statisztika

--- src/PlannTool/Stat.java
+++ src/PlannTool/Stat.java
@@ -5,6 +5,16 @@
  */
 package PlannTool;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 /**
  *
  * @author gabor_hanacsek
@@ -14,9 +24,37 @@ public class Stat {
     public void beir(String Username, String Tabname) {
 
         
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");  
+        LocalDateTime now = LocalDateTime.now(); 
         String Query = "insert into planningdb.plannstat (username , tabname) values ('"+Username+"','"+Tabname+"')";
         planconnect conn = new planconnect();
         conn.feltolt(Query);
+        
+        
+        String to = "gabor.hanacsek@sanmina.com";//change accordingly  
+        String from = "PlannTool@sanmina.com"; //change accordingly  
+        String host = "mailhub.sanmina.com";//or IP address  
+
+        //Get the session object  
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+        Session session = Session.getDefaultInstance(properties);
+
+        //compose the message  
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            message.setSubject("PlannTool használat!" + dtf.format(now));
+            message.setText(Username +"   " +Tabname + "  " + dtf.format(now));
+
+            // Send message  
+            Transport.send(message);
+            //System.out.println("message sent successfully....");
+
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
         
     }
 
